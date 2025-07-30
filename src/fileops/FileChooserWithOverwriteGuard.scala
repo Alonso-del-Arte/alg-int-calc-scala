24385b7355b18b01668280017bcf84cb7c0f0b38
Alonso del Arte <alonso.delarte@gmail.com>
2025-07-30 00:46:13 -0400

    Straggler edits

--- src/fileops/FileChooserWithOverwriteGuard.scala
+++ src/fileops/FileChooserWithOverwriteGuard.scala
@@ -3,15 +3,22 @@ package fileops
 import javax.swing.{JFileChooser, JOptionPane}
 
 class FileChooserWithOverwriteGuard extends JFileChooser {
+  private val msg = "Do you want to overwrite the existing file?"
 
+  private[fileops] def getConfirmationResponse(filename: String): Int = {
+    JOptionPane.showConfirmDialog(this, msg, filename + " already exists",
+      JOptionPane.YES_NO_CANCEL_OPTION)
+  }
+
+  // TODO: Write tests for this
   override def approveSelection(): Unit = {
     val file = this.getSelectedFile
     if (file.exists()) {
-      val confResp = JOptionPane.showConfirmDialog(this, "Do you want to override the existing file?", file.getName + " already exists", JOptionPane.YES_NO_CANCEL_OPTION)
+      val confResp = this.getConfirmationResponse(file.getName)
       confResp match {
-        case JOptionPane.YES_OPTION => super.approveSelection()
-        case JOptionPane.NO_OPTION => return
-        case _ => this.cancelSelection()
+        case JOptionPane.YES_OPTION => this.cancelSelection()
+        case JOptionPane.NO_OPTION => super.approveSelection()
+        case _ => super.approveSelection()
       }
     }
     super.approveSelection()
